84f3dc5336335ce2fe0185504975f05568e402e8
Mike Hepburn <eformat@gmail.com>
2022-07-29 08:38:34 +1000

    🦒 quarkus 2.11.1.Final 🦒

--- src/test/java/org/acme/ExampleResourceIT.java
+++ src/test/java/org/acme/ExampleResourceIT.java
@@ -4,6 +4,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class ExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
+    // Execute the same tests but in packaged mode.
 }
